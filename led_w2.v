dc317b6ac9b419995e95a7e662fd03b04e180990
LaZoark <emersonchen086@icloud.com>
2021-03-30 18:02:29 +0800

    improve the circuit logically...

--- led_w2.v
+++ led_w2.v
@@ -20,14 +20,26 @@ module led_w2(
 	//////////// SW //////////
 	SW,
 
+//	//////////// SEG7 //////////
+//	HEX0,
+//	HEX1,
+//	HEX2,
+//	HEX3,
+//	HEX4,
+//	HEX5,
+//	HEX6,
+//	HEX7,
+//
+//	//////////// GPIO, GPIO connect to GPIO Default //////////
+//	GPIO 
 );
 
 //=======================================================
 //  PARAMETER declarations
 //=======================================================
 
-  // nope, I'm gonna place it below
-
+// nope, I'm gonna place it below
+	
 //=======================================================
 //  PORT declarations
 //=======================================================
@@ -39,7 +51,7 @@ input 		          		CLOCK3_50;
 
 //////////// LED //////////
 //output		     [8:0]		LEDG;
-output reg   	     [8:0]		LEDG;
+output reg   	     [8:0]		LEDG = 8'b0_0011_1000;
 output reg		    [17:0]		LEDR;
 
 //////////// KEY //////////
@@ -47,6 +59,19 @@ input 		     [3:0]		KEY;
 
 //////////// SW //////////
 input 		    [17:0]		SW;
+
+//////////// SEG7 //////////
+//output		     [6:0]		HEX0;
+//output		     [6:0]		HEX1;
+//output		     [6:0]		HEX2;
+//output		     [6:0]		HEX3;
+//output		     [6:0]		HEX4;
+//output		     [6:0]		HEX5;
+//output		     [6:0]		HEX6;
+//output		     [6:0]		HEX7;
+//
+////////////// GPIO, GPIO connect to GPIO Default //////////
+//inout 		    [35:0]		GPIO;
 
 
 //=======================================================
@@ -61,29 +86,26 @@ reg				[32:0]		counter_inc;
 //=======================================================
 //  Structural coding
 //=======================================================
-parameter shift_time = 32'd25_000_000;     	// determintion of the shift time
-parameter shape_led  = 8'b0011_1000;			// to config the shape of leds
+
+
+parameter shift_time = 32'd24_999_999;     	// determintion of the shift time
 always @(posedge CLOCK_50)
 begin
 		counter_inc <= counter_inc + 1'b1;		// tell <space>'<amount of bit>
 
-		key0diy <= {key0diy[1:0] + !KEY[3]};			// setting the shape of led
+		key0diy <= {key0diy[1:0] ,KEY[3]};			// setting the shape of led	
 		if (!key0diy[2]&&key0diy[1])
 			LEDG[7:0] <= SW[7:0];
-			
-		if (counter_inc == 32'd49_999_999)		// for 50MHz, it means 1 seconds!
-			LEDG[7:0] <= shape_led;					// initiallizing
 			
 		if (LEDG[7])
 			LEDR[0] <= 1'b1;
 		else if (LEDG[0])
 			LEDR[0] <= 1'b0;
-
-		if(LEDR[0])begin
-			if (counter_inc % shift_time == 0)
-				LEDG <= LEDG >> 1;					//right shift, when redLED is HIGH
-		end else if(!LEDR[0])
-			if (counter_inc % shift_time == 0)
-				LEDG <= LEDG << 1;					//left shift, when redLED is LOW
+			
+		if (counter_inc == shift_time)begin
+			LEDG <= LEDR[0] ? LEDG >> 1 : LEDG << 1;	// LEDR[0] decides left/right shifting
+			counter_inc <= 0;
+		end
+			
 end
 endmodule
